02c6220085864cf9be8520cbce152a0abe561214
SA6MWA Michel <sa6mwa@gmail.com>
2024-11-29 01:27:29 +0100

    Add newline at end of string

--- putFile.go
+++ putFile.go
@@ -3,7 +3,7 @@ package fileops
 import "os"
 
 func PutFile(textfile string, content string, perm os.FileMode) error {
-	if err := os.WriteFile(textfile, []byte(content), perm); err != nil {
+	if err := os.WriteFile(textfile, []byte(content+`\n`), perm); err != nil {
 		return err
 	}
 	return os.Chmod(textfile, perm)
